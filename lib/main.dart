dbdb39120d6c0980d609ccdead0d02047bb94ac0
iamplatano <bestuas_x15@hotmail.com>
2022-02-21 10:30:11 -0500

    Added one liner

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _SafariAppState extends State<SafariApp> {
           backgroundColor: Color.fromARGB(255, 37, 23, 21),
           appBar: AppBar(
             title: Text(
-              "Safari Look up",
+              "Safari Gallery",
               style: TextStyle(
                 color: Colors.black,
               ),
@@ -34,58 +34,56 @@ class _SafariAppState extends State<SafariApp> {
             centerTitle: true,
             backgroundColor: Color.fromARGB(255, 204, 159, 26),
           ),
-          body: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: IconButton(
-                      iconSize: 50,
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          currentind -= 1;
-                          if (currentind < 0) {
-                            currentind = images.length - 1;
-                          }
-                        });
-                      },
-                      icon: Icon(Icons.navigate_before_rounded)),
-                ),
-                Column(
-                  children: <Widget>[
-                    Image(
-                      image: AssetImage(images[currentind]),
-                      width: 450,
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                child: IconButton(
+                    iconSize: 50,
+                    color: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        currentind -= 1;
+                        if (currentind < 0) {
+                          currentind = images.length - 1;
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.navigate_before_rounded)),
+              ),
+              Column(
+                children: <Widget>[
+                  Image(
+                    image: AssetImage(images[currentind]),
+                    width: 450,
+                  ),
+                  Divider(height: 20),
+                  SizedBox(
+                    height: 150,
+                    width: 550,
+                    child: Text(
+                      descriptions[currentind],
+                      style: TextStyle(color: Colors.white, fontSize: 18),
                     ),
-                    Divider(height: 20),
-                    SizedBox(
-                      height: 100,
-                      width: 550,
-                      child: Text(
-                        descriptions[currentind],
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    )
-                  ],
-                ),
-                Expanded(
-                  child: IconButton(
-                      iconSize: 50,
-                      color: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          currentind += 1;
-                          if (currentind > images.length - 1) {
-                            currentind = 0;
-                          }
-                        });
-                      },
-                      icon: Icon(Icons.navigate_next_rounded)),
-                ),
-              ],
-            ),
+                  )
+                ],
+              ),
+              Expanded(
+                child: IconButton(
+                    iconSize: 50,
+                    color: Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        currentind += 1;
+                        if (currentind > images.length - 1) {
+                          currentind = 0;
+                        }
+                      });
+                    },
+                    icon: Icon(Icons.navigate_next_rounded)),
+              ),
+            ],
           ),
         ));
   }
